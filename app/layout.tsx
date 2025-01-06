020b8660521c036bfaaa748ad881469809cd64e4
Matthias Holetzko <mholetzko@gmx.net>
2025-01-06 05:07:57 +0100

    feat: prep light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Header } from "./components/Header"
+import { ThemeProvider } from "./components/ThemeProvider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -16,12 +17,14 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Header />
-        <main className="pt-16">
-          {children}
-        </main>
+        <ThemeProvider>
+          <Header />
+          <main className="pt-16">
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   )
